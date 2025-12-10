6156023cb1be6bc0ead24a6854b1f6cd3e1f8ddc
Carla <carla.piedra3@gmail.com>
2025-12-10 20:12:33 +0100

    Initial commit - setup project and install Clerk + Supabase

---
+++ lib/supabase/server.ts
@@ -0,0 +1,39 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createClient = () => {
+    const cookieStore = cookies()
+
+    if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+        throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+    }
+    if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+        throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+    }
+
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value, ...options })
+                    } catch (error) {
+                        // Handle the error if needed
+                    }
+                },
+                remove(name: string, options: CookieOptions) {
+                    try {
+                        cookieStore.set({ name, value: '', ...options })
+                    } catch (error) {
+                        // Handle the error if needed
+                    }
+                },
+            },
+        }
+    )
+}
